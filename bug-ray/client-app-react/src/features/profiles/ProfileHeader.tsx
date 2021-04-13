13019429491bba04f109f2e9eabc60842bf59762
Zeno3092 <sunilg3011992@gmail.com>
2021-04-13 11:49:05 +0530

    BR - UI Work in progress

--- bug-ray/client-app-react/src/features/profiles/ProfileHeader.tsx
+++ bug-ray/client-app-react/src/features/profiles/ProfileHeader.tsx
@@ -20,51 +20,61 @@ const ProfileHeader = ({ profile }: Props) => {
   return (
     <Segment>
       <Grid>
-        <Grid.Column width={12}>
+        <Grid.Row centered>
           <Item.Group>
             <Item>
               <Item.Image
-                avatar
+                bordered
+                circular
                 size="small"
                 src={profile.image || "/assets/user.png"}
               />
+            </Item>
+          </Item.Group>
+        </Grid.Row>
+        <Grid.Row centered>
+          <Item.Group>
+            <Item>
               <Item.Content verticalAlign="middle">
                 <Header as="h1" content={profile.displayName} />
               </Item.Content>
             </Item>
           </Item.Group>
-        </Grid.Column>
-        <Grid.Column width={4}>
-          <Statistic.Group widths={2}>
-            <Statistic>
-              <Statistic.Value>1</Statistic.Value>
-              <Statistic.Label>Followers</Statistic.Label>
-            </Statistic>
-            <Statistic>
-              <Statistic.Value>5</Statistic.Value>
-              <Statistic.Label>Following</Statistic.Label>
-            </Statistic>
-            {/* <Statistic>
+        </Grid.Row>
+        <Divider />
+        <Grid.Row>
+          <Grid.Column width={16}>
+            <Statistic.Group widths={2}>
+              <Statistic>
+                <Statistic.Value>1</Statistic.Value>
+                <Statistic.Label>Followers</Statistic.Label>
+              </Statistic>
+              <Statistic>
+                <Statistic.Value>5</Statistic.Value>
+                <Statistic.Label>Following</Statistic.Label>
+              </Statistic>
+              {/* <Statistic>
               <Statistic.Value>42</Statistic.Value>
               <Statistic.Label>Teamsize/Contributors</Statistic.Label>
             </Statistic> */}
-          </Statistic.Group>
+            </Statistic.Group>
 
-          <Divider />
-          <Reveal animated="move">
-            <Reveal.Content visible style={{ width: "100%" }}>
-              <Button fluid color="blue" content="Follow" />
-            </Reveal.Content>
-            <Reveal.Content hidden style={{ width: "100%" }}>
-              <Button
-                fluid
-                basic
-                color={true ? "red" : "green"}
-                content="Unfollow"
-              />
-            </Reveal.Content>
-          </Reveal>
-        </Grid.Column>
+            <Divider />
+            <Reveal animated="move">
+              <Reveal.Content visible style={{ width: "100%" }}>
+                <Button fluid color="blue" content="Follow" />
+              </Reveal.Content>
+              <Reveal.Content hidden style={{ width: "100%" }}>
+                <Button
+                  fluid
+                  basic
+                  color={true ? "red" : "green"}
+                  content="Unfollow"
+                />
+              </Reveal.Content>
+            </Reveal>
+          </Grid.Column>
+        </Grid.Row>
       </Grid>
     </Segment>
   );
