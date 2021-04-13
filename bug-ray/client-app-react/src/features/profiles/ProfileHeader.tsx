c83999024d83b3fadb5ce271a2ffcd646d9fe8cf
Zeno3092 <sunilg3011992@gmail.com>
2021-04-13 10:03:53 +0530

    BR - Profile WIP

--- bug-ray/client-app-react/src/features/profiles/ProfileHeader.tsx
+++ bug-ray/client-app-react/src/features/profiles/ProfileHeader.tsx
@@ -10,17 +10,26 @@ import {
   Segment,
   Statistic,
 } from "semantic-ui-react";
+import { Profile } from "../../app/models/profile";
 
-const ProfileHeader = () => {
+interface Props {
+  profile: Profile;
+}
+
+const ProfileHeader = ({ profile }: Props) => {
   return (
     <Segment>
       <Grid>
         <Grid.Column width={12}>
           <Item.Group>
             <Item>
-              <Item.Image avatar size="small" src={"/assets/user.png"} />
+              <Item.Image
+                avatar
+                size="small"
+                src={profile.image || "/assets/user.png"}
+              />
               <Item.Content verticalAlign="middle">
-                <Header as="h1" content="DisplayName" />
+                <Header as="h1" content={profile.displayName} />
               </Item.Content>
             </Item>
           </Item.Group>
--- bug-ray/client-app-react/src/features/profiles/ProfilePage.tsx
+++ bug-ray/client-app-react/src/features/profiles/ProfilePage.tsx
@@ -1,14 +1,28 @@
 import { observer } from "mobx-react-lite";
-import React from "react";
+import React, { useEffect } from "react";
+import { useParams } from "react-router";
 import { Grid } from "semantic-ui-react";
+import LodingComponet from "../../app/layout/LodingComponet";
+import { useStore } from "../../app/stores/store";
 import ProfileContent from "./ProfileContent";
 import ProfileHeader from "./ProfileHeader";
 
 const ProfilePage = () => {
+  //getting values form  Route params
+  const { username } = useParams<{ username: string }>();
+  const { profileStore } = useStore();
+  const { loadingProfile, loadProfile, profile } = profileStore;
+
+  useEffect(() => {
+    loadProfile(username);
+  }, [loadProfile, username]);
+
+  if (loadingProfile) return <LodingComponet content="Loading Profile..." />;
+
   return (
     <Grid>
       <Grid.Column width={16}>
-        <ProfileHeader />
+        {profile && <ProfileHeader profile={profile} />}
         <ProfileContent />
       </Grid.Column>
     </Grid>
